bacad12b52c82d396c3a5fc1ebda6ff68a8bded5
Natan Oliveira <natannjos@gmail.com>
2019-07-13 11:34:21 -0300

    Add token authentication on settings

--- bikeunit_api/settings.py
+++ bikeunit_api/settings.py
@@ -115,3 +115,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',  # <-- And here
+    ],
+}
